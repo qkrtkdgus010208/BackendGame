926d487ead60fdf17969db5295607dee41a1bcf2
qkrtkdgus010208 <psh010208@naver.com>
2025-06-28 18:34:41 +0900

    뒤끝 강의 #08
    랭킹 불러오기와 랭킹 UI 출력

--- Assets/Scripts/#00Common/Constants.cs
+++ Assets/Scripts/#00Common/Constants.cs
@@ -2,5 +2,6 @@
 {
 	public static readonly string	USER_DATA_TABLE	= "USER_DATA";
 	public static readonly string	DAILY_RANK_UUID	= "0197a7b7-0da5-7e38-aaeb-c33f557da616";
+    public static readonly int		MAX_RANK_LIST = 20;
 }
 
